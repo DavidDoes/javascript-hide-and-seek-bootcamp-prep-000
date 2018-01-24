4d001169bee622c6f427e34e1e7af00ef90cd610
David Clark <dlc.clark@gmail.com>
2018-01-24 03:32:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild(){
+  var node = document.getElementById("grand-node").querySelectorAll("div");
+  var child;
+  for (let i = 0; i < node.length-1; i++){
+    child = node[i].querySelector("div")
+  }
+return child;
+}
